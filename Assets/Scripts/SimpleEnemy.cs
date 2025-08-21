b7f95d4cb1fa9d0256a654b7c4af21e8799f6353
SimonCotterill <m0nyp3nny@gmail.com>
2025-08-21 16:42:25 -0400

    changed player and enemy ship scales, made enemy ship prefab variants, added enemy hitpoints

--- Assets/Scripts/SimpleEnemy.cs
+++ Assets/Scripts/SimpleEnemy.cs
@@ -4,11 +4,24 @@ public class SimpleEnemy : MonoBehaviour
 {
 
     [SerializeField] GameObject destroyedVFX;
-    
+    [SerializeField] int hitPoints = 3;
+
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        //or hitpoints--;
+        //or change '1' to be a variable 'damage'
+
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             //Quaternion.identity just means no rotation
-        Destroy(this.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }

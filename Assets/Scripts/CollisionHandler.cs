0deb9d133f40b70f21b69e326c0e96e6ab41475f
SimonCotterill <m0nyp3nny@gmail.com>
2025-08-21 12:14:42 -0400

    instantiating particle effects with destructions

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
 
+    [SerializeField] GameObject destroyedVFX;
+
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+
         UnityEngine.Debug.Log("Hit " + other.name);
         //Or UnityEngine.Debug.Log($"Hit {other.gameObject.name}");
     }
